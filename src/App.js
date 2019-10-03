0c22daf0c8e83eac375e206acd2be29b02f1b712
carloslazo <clz1104@gmail.com>
2019-10-02 17:22:08 -0700

    select dropdown initial for body

--- src/App.js
+++ src/App.js
@@ -6,13 +6,46 @@ import bottom from './bottom.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      "selected": "turtleNeck"
+    }
+    this.handleClick = this.handleClick.bind(this);
   }
   
+  defineSelectList(part){
+    return Object.keys(part).map(function(key, index) {
+      return key
+    })
+  }
   
-  render(){  
-    console.log(bottom);  
+  handleClick(e){
+   let value = e.target.value
+   this.setState({"selected": value})
+  }
+  
+  render(){
     return (
-      <div>{body.turtleNeck}</div>
+    <div>
+      <div>
+        <div>{body[this.state.selected]}</div>
+        <div>{bottom.standing.sweeatpants}</div>
+      </div>
+      
+      <div>
+        <div class="select">
+          <select
+            onChange={this.handleClick}
+            >
+            <option>Select body</option>
+            {
+              this.defineSelectList(body).map( key => {
+                return <option>{key}</option>
+              } )
+            }
+          </select>
+        </div>
+      </div>
+    </div>
     );
   }
 }
